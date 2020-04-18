c542db10da982c6df5ba378b7d812b9c50ee9bb0
Nashmia Riaz <nashmia-riaz@hotmail.com>
2020-04-18 23:03:16 +0100

    Added source files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# spotify-current-playing-for-OBS
+# Now Playing Spotify for OBS
+
+A fully customisable node.js based browser source for OBS that fetches the users current playing song.
+
+To run, you must follow and setup your app as developer on Spotify. Follow this [tutorial](https://developer.spotify.com/dashboard/). You also need to have node js already installed:
+
+1. Add your project on Spotify for Developers dashboard. You can name it whatever you like.
+2. Copy down the client ID and client secret. For example, in my set up for the project called OBS Now Playing, I have:\
+![client ID and client secret](images/client_stuff.png)
+3. Replace these in app.js. Note that you need to add `http://localhost:8888/callback` and `http://localhost:8888` to the Redirect URIs in the Developers Dashboard. You can find these under Edit Settings.
+4. Now, run `npm install` in this repo folder. Once the dependencies are done installing, run `node app.js`.
+5. If all goes well, it should say 'Listening on 8888' in your console. Now open up localhost:8888 in browser, sign in and grant access.
+6. You should see the now playing song in the browser:\
+![now playing preview](images/now_playing_preview.png)
+7. Copy the link generated in the browser and add that as a browser source in OBS. Optimal size is 450x150.
+To modify how the now playing looks in the browser, you can play around with [styles.css](public/styles.css).
---
+++ public/script.js
@@ -0,0 +1,71 @@
+
+  (function() {
+
+    /**
+     * Obtains parameters from the hash of the URL
+     * @return Object
+     */
+    function getHashParams() {
+      var hashParams = {};
+      var e, r = /([^&;=]+)=?([^&;]*)/g,
+          q = window.location.hash.substring(1);
+      while ( e = r.exec(q)) {
+         hashParams[e[1]] = decodeURIComponent(e[2]);
+      }
+      return hashParams;
+    }
+    var params = getHashParams();
+
+    var access_token = params.access_token,
+        refresh_token = params.refresh_token,
+        error = params.error;
+
+    var currentSong="";
+    var currentArtist = "";
+
+    var song=""; var artist = ""; var albumArt = "";
+
+    if (error) {
+      alert('There was an error during the authentication');
+    } else {
+      if (access_token) {
+
+        $('#login').hide();
+        $('#container').show();
+
+        function fetchDetails(){
+          $.ajax({
+              url: 'https://api.spotify.com/v1/me/player/currently-playing',
+              headers: {
+                'Authorization': 'Bearer ' + access_token
+              },
+              success: function(response) {
+                console.log(response);
+                if(response.is_playing && ((currentSong != song && currentArtist != artist)
+                  || (currentSong == "" && currentArtist == ""))){
+                    
+                  song = response.item.name;
+                  artist = response.item.artists[0].name;
+                  albumArt = response.item.album.images[0].url;
+
+                  $("#song").html(song);
+                  $("#artist").html(artist);
+                  $("#albumArt").attr("src", albumArt);
+                  $('#container').fadeIn();
+                }
+                else{
+                  $('#container').fadeOut();
+                }
+              }
+          });
+        }
+        fetchDetails();
+        setInterval(fetchDetails, 1500);
+      } else {
+        $('#container').fadeOut();
+        // render initial screen
+        $('#login').show();
+        $('#container').hide();
+      }
+    }
+  })();
